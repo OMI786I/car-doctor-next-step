a2d6f219917cfb47c6cd7838c57d2f2e65b3fd34
OMI786I <sohrawardy1998@gmail.com>
2024-09-09 22:56:45 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hello Car Doctor
+      <button className="btn">Click ME!</button>
+    </div>
+  );
+}
