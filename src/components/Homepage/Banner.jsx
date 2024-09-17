1003796409f481e72129eea06e45cc0ac3522c81
OMI786I <sohrawardy1998@gmail.com>
2024-09-17 23:46:41 +0600

    dne with banner section

--- src/components/Homepage/Banner.jsx
+++ src/components/Homepage/Banner.jsx
@@ -2,44 +2,61 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="carousel relative w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        {/**hero */}
+    <div className="carousel w-full relative mt-12">
+      {banners.map((res, index) => (
         <div
-          className="hero min-h-screen"
-          style={{
-            backgroundImage:
-              "linear-gradient(45deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.3)), url('https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp')",
-          }}
+          key={res.id}
+          id={`slide${res.id}`}
+          className="carousel-item relative w-full"
         >
-          <div className="hero-content text-neutral-content text-center">
-            <div className="max-w-md">
-              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-              <p className="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
-              <button className="btn btn-primary">Get Started</button>
+          {/**hero */}
+          <div
+            className="hero min-h-screen"
+            style={{
+              backgroundImage: `linear-gradient(45deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.3)), url("assets/images/banner/${
+                index + 1
+              }.jpg")`,
+            }}
+          >
+            <div className="hero-content text-neutral-content text-left">
+              <div className="max-w-md">
+                <h1 className="mb-5 text-5xl font-bold">{res.title}</h1>
+                <p className="mb-5">{res.description}</p>
+                <div className="flex gap-3">
+                  <button className="btn btn-primary">Discover More</button>
+                  <button className="outline outline-offset-1 outline-white p-2 rounded-md text-white hover:bg-black hover:duration-200 hover:outline-none transition ease-in-out  ">
+                    Latest Projects
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
+          {/**hero */}
+
+          {/** Navigation buttons */}
+          <div className="absolute flex justify-between transform -translate-y-1/2 gap-2 right-5 top-[80%]">
+            <a
+              href={res.prev}
+              className="btn btn-outline btn-primary btn-circle"
+            >
+              ❮
+            </a>
+            <a
+              href={res.next}
+              className="btn btn-outline btn-primary btn-circle"
+            >
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
-      {/**hero */}
-      <div className="absolute inset-x-0 bottom-4 flex justify-end">
-        <a href="#slide4" className="btn btn-circle">
-          ❮
-        </a>
-        <a href="#slide2" className="btn btn-circle ml-4">
-          ❯
-        </a>
-      </div>
+      ))}
     </div>
   );
 };
 
 const banners = [
   {
+    id: 1,
     title: "Affordable Price for Car Servicing",
     description:
       "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
@@ -47,6 +64,7 @@ const banners = [
     prev: "#slide6",
   },
   {
+    id: 2,
     title: "Affordable Price for Car Servicing",
     description:
       "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
@@ -54,6 +72,7 @@ const banners = [
     prev: "#slide1",
   },
   {
+    id: 3,
     title: "Affordable Price for Car Servicing",
     description:
       "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
@@ -61,6 +80,7 @@ const banners = [
     prev: "#slide2",
   },
   {
+    id: 4,
     title: "Affordable Price for Car Servicing",
     description:
       "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
@@ -68,6 +88,7 @@ const banners = [
     prev: "#slide3",
   },
   {
+    id: 5,
     title: "Affordable Price for Car Servicing",
     description:
       "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
@@ -75,18 +96,12 @@ const banners = [
     prev: "#slide4",
   },
   {
+    id: 6,
     title: "Affordable Price for Car Servicing",
     description:
       "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
     next: "#slide1",
-    prev: "#slide4",
-  },
-  {
-    title: "Affordable Price for Car Servicing",
-    description:
-      "Our skilled technicians use the latest tools and genuine parts to ensure your car runs smoothly and safely. From routine oil changes and tire rotations to comprehensive inspections and repairs, we offer a range of services tailored to meet your needs and budget.",
-    next: "#slide2",
-    prev: "#slide4",
+    prev: "#slide5",
   },
 ];
 
