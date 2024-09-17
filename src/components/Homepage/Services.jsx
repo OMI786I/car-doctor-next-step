dc126b56cb85eca63c45abb3339c95c70e78d367
OMI786I <sohrawardy1998@gmail.com>
2024-09-18 00:39:58 +0600

    getting started services section

---
+++ src/components/Homepage/Services.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <div className="text-center space-y-4">
+        <h1 className="text-primary font-bold">Service</h1>
+        <h1 className="text-2xl font-bold">Our Service Area</h1>
+        <p className="text-gray-600">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.{" "}
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Services;
